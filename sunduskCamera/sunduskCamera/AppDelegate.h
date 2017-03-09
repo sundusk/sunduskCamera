abe2e3e03c5a1eacf8c6ed402bf8684a13205f0c
sundusk <zhuyue346@gmail.com>
2017-03-09 18:54:13 +0800

    添加GPUimage第三个库

---
+++ sunduskCamera/sunduskCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sunduskCamera
+//
+//  Created by sundusk on 2017/3/9.
+//  Copyright © 2017年 sundusk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
