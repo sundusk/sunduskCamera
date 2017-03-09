18f61abdfcb9153c51dd5b06e9d0c26991421a2a
sundusk <zhuyue346@gmail.com>
2017-03-09 20:09:15 +0800

    创建项目

--- sunduskCamera/Pods/Target Support Files/Pods-sunduskCameraUITests/Pods-sunduskCameraUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sunduskCameraUITests : NSObject
-@end
-@implementation PodsDummy_Pods_sunduskCameraUITests
-@end
---
+++ sunduskCamera/sunduskCamera/CCQCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCQCameraViewController.h
+//  sunduskCamera
+//
+//  Created by sundusk on 2017/3/9.
+//  Copyright © 2017年 sundusk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCQCameraViewController : UIViewController
+
+@end
